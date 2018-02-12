edf146dca38700f3f5ba4945524903e24264b8cb
Muhtarudin Siregar ツ <redcar.studious@gmail.com>
2018-02-12 14:56:11 +0700

    Added intent detail + make detail recipe page

--- app/src/main/java/com/example/ardin/alltherecipes/MainActivity.kt
+++ app/src/main/java/com/example/ardin/alltherecipes/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.ardin.alltherecipes
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
     lateinit var mListView: ListView
-
+    val context: Context? = null;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,5 +24,24 @@ class MainActivity : AppCompatActivity() {
         val adapter = RecipeAdapter(this, recipeList)
         mListView = recipe_list_view
         mListView.adapter = adapter
+
+        mListView.onItemClickListener = object : AdapterView.OnItemClickListener {
+
+            override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                // 1 get recipe by position/index
+                val selectedRecipe = recipeList[position]
+
+                // 2 create intent to navigate to RecipeDetail
+                val detailIntent = Intent(context, RecipeDetailActivity::class.java)
+
+                // 3 let RecipeDetailActivity know title and url to display by passing data via intent
+                detailIntent.putExtra("title", selectedRecipe.title)
+                detailIntent.putExtra("url", selectedRecipe.instructionUrl)
+
+                // 4 start/launch the actifity(RecipeDetailActivity)
+                startActivity(detailIntent)
+            }
+
+        }
     }
 }
